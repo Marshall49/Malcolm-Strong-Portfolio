dec85453870c17733ba1651a74b0811f2170db5f
Malcolm Strong <strong.malcolm@yahoo.com>
2017-10-16 12:57:18 -0400

    added sme styling

---
+++ Portfolio/Assets/Javascript/click.js
@@ -0,0 +1,23 @@
+$(".button").on('click', function(){
+    $("#services").css("display", "block");		//displays the previously hidden container class block
+    lastElementTop = $('#services').position().top ; //finds the position of the container class and assigns to variable
+    $('html, body').animate({scrollTop: lastElementTop}, 'slow'); //scrolls to the position found in last line
+});
+
+$(".links-port").on('click', function(){
+    //jQuery function which triggers when developers link clicked
+    $("#services").css("display", "block");		//displays the previously hidden container class block
+    lastElementTop = $('#services').position().top ; //finds the position of the container class and assigns to variable
+    $('html, body').animate({scrollTop: lastElementTop}, 'slow'); //scrolls to the position found in last line
+});
+
+$(".links-about").on('click', function(){
+    //jQuery function which triggers when developers link clicked
+    $(".about-me").css("display", "block");		//displays the previously hidden container class block
+    lastElementTop = $('.about-me').position().top ; //finds the position of the container class and assigns to variable
+    $('html, body').animate({scrollTop: lastElementTop}, 'slow'); //scrolls to the position found in last line
+});
+
+$('#logo').on('click', function(){					//jQuery function which triggers when camp icon is clicked
+    $('html, body').animate({ scrollTop: 0 }, 'slow'); //scrolls to position 0 (top of page)
+})
