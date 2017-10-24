bb8aff15cf3a777f7b0aad8a96e90200ab2a07ee
Malcolm Strong <strong.malcolm@yahoo.com>
2017-10-24 19:02:46 -0400

    added some styling

--- Portfolio/Assets/Javascript/click.js
+++ Portfolio/Assets/Javascript/click.js
@@ -18,6 +18,14 @@ $(".links-about").on('click', function(){
     $('html, body').animate({scrollTop: lastElementTop}, 'slow'); //scrolls to the position found in last line
 });
 
+
+$(".links-contact").on('click', function(){
+    //jQuery function which triggers when developers link clicked
+    $(".about-me").css("display", "block");		//displays the previously hidden container class block
+    lastElementTop = $('.about-me').position().top ; //finds the position of the container class and assigns to variable
+    $('html, body').animate({scrollTop: lastElementTop}, 'slow'); //scrolls to the position found in last line
+});
+
 $('#logo').on('click', function(){					//jQuery function which triggers when camp icon is clicked
     $('html, body').animate({ scrollTop: 0 }, 'slow'); //scrolls to position 0 (top of page)
 })
