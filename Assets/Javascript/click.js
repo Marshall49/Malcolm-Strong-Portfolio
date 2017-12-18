2a5b932eae0b37d6597a08355193bb02ef793743
Malcolm Strong <strong.malcolm@yahoo.com>
2017-12-18 07:41:23 -0500

    final changes

--- Assets/Javascript/click.js
+++ Assets/Javascript/click.js
@@ -1,29 +1,45 @@
+//Navbar Color Change Function
+$(document).ready(function(){
+  var scroll_start = 0;
+  var button = $('.button');
+  var offset = button.offset();
+    if (button.length) {
+    $(document).scroll(function() {
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+        $('.navbar').css('background', 'url("Assets/Images/superhero.png")');
+        $('.navbar').css('1s');
+      } else {
+        $('.navbar').css('background' , 'none');
+      }
+    });
+  }
+});
+
+//Click Buttons
+
 $(".button").on('click', function(){
-    $(".container").css("display", "block");		//displays the previously hidden container class block
-    lastElementTop = $('.container').position().top ; //finds the position of the container class and assigns to variable
-    $('html, body').animate({scrollTop: lastElementTop}, 'slow'); //scrolls to the position found in last line
+    $('html, body').animate({
+        scrollTop: $('.container').offset().top
+      }, 1000); //scrolls to the position found in last line
 });
 
 $(".projects").on('click', function(){
-    //jQuery function which triggers when developers link clicked
-    $(".container").css("display", "block");		//displays the previously hidden container class block
-    lastElementTop = $('.container').position().top ; //finds the position of the container class and assigns to variable
-    $('html, body').animate({scrollTop: lastElementTop}, 'slow'); //scrolls to the position found in last line
+    $('html, body').animate({
+        scrollTop: $('.container').offset().top
+      }, 1000); //scrolls to the position found in last line
 });
 
 $(".about").on('click', function(){
-    //jQuery function which triggers when developers link clicked
-    $(".click-contact").css("display", "block");		//displays the previously hidden container class block
-    lastElementTop = $('.click-contact').position().top ; //finds the position of the container class and assigns to variable
-    $('html, body').animate({scrollTop: lastElementTop}, 'slow'); //scrolls to the position found in last line
+    $('html, body').animate({
+        scrollTop: $('.mal-fluid').offset().top
+      }, 2000); //scrolls to the position found in last line
 });
 
-
 $(".contact").on('click', function(){
-    //jQuery function which triggers when developers link clicked
-    $(".click-contact").css("display", "block");		//displays the previously hidden container class block
-    lastElementTop = $('.click-contact').position().top ; //finds the position of the container class and assigns to variable
-    $('html, body').animate({scrollTop: lastElementTop}, 'slow'); //scrolls to the position found in last line
+    $('html, body').animate({
+        scrollTop: $('.about-title').offset().top
+      }, 2000); //scrolls to the position found in last line
 });
 
 $('#logo').on('click', function(){					//jQuery function which triggers when camp icon is clicked
