10f4cca5969561f8c2e4dee5756b24b9b0b94575
Malcolm Strong <strong.malcolm@yahoo.com>
2017-12-27 08:01:43 -0500

    animation change

--- Assets/Javascript/click.js
+++ Assets/Javascript/click.js
@@ -8,12 +8,12 @@ $(document).ready(function(){
       scroll_start = $(this).scrollTop();
       if(scroll_start > offset.top) {
         $('.navbar').css('background', 'url("Assets/Images/superhero.png")');
-        $('.projects').css({"-webkit-animation": "fadeIn 15s", "animation": "fadeIn 15s"});
-        $('.about').css({"-webkit-animation": "fadeIn 15s", "animation": "fadeIn 15s"});
-        $('.contact').css({"-webkit-animation": "fadeIn 15s", "animation": "fadeIn 15s"});
+        $('.projects').css({"-webkit-animation": "fadeIn 30s", "animation": "fadeIn 30s"});
+        $('.about').css({"-webkit-animation": "fadeIn 30s", "animation": "fadeIn 30s"});
+        $('.contact').css({"-webkit-animation": "fadeIn 30s", "animation": "fadeIn 30s"});
         // $('#logo').css({"-webkit-animation": "fadeIn 15s", "animation": "fadeIn 15s"});
-        $('.Header').css({"-webkit-animation": "animated rubberBand 10s", "animation": "animated rubberBand 10s"});
-        $('.links').css({"border-radius": "none", "margin-top": "-5px", "margin-right": "-5px", "margin-bottom": "-1px", "box-shadow": "inset -80px 0px 100px 1px #ABABAB, 5px 5px 100px -95px #DDDDDD"});
+        // $('.Header').css({"-webkit-animation": "animated rubberBand 10s", "animation": "animated rubberBand 10s"});
+        // $('.links').css({"border-radius": "none", "margin-top": "-5px", "margin-right": "-5px", "margin-bottom": "-1px", "box-shadow": "inset -80px 0px 100px 1px #ABABAB, 5px 5px 100px -95px #DDDDDD"});
         // $('#logo').css({"border-radius": "none", "margin-top": "3px", "margin-left": "-5px", "margin-bottom": "-50px", "box-shadow": "inset 2px 0px 100px 1px #ABABAB, 5px 5px 100px -95px #DDDDDD"});
         // $('.name').css('display' , 'none');
       } else {
@@ -21,7 +21,7 @@ $(document).ready(function(){
         $('.projects').css('animation' , 'none');
         $('.about').css('animation' , 'none');
         $('.contact').css('animation' , 'none');
-        $('.links').css('box-shadow' , 'none');
+        // $('.links').css('box-shadow' , 'none');
         // $('#logo').css('box-shadow' , 'none');
         // $('.name').css('display' , 'block');
       }
